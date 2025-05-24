3b89acac49ff3c1262f625b75210c9da5aa025b0
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-05-24 17:02:27 +0200

    Add Brand to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BrandsModule } from './brands/brands.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/WrenchRider'),
+    BrandsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

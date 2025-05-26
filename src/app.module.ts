ceabb2f37cae44f6c204627331340433d4422bc3
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-05-26 22:46:45 +0200

    Import module and .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { BrandsModule } from './brands/brands.module';
+import { BrandModule } from './brand/brand.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/WrenchRider'),
-    BrandsModule,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    BrandModule,
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
